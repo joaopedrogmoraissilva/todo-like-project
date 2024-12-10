34b9ec9c57dcb95062392ce022539ac1238d9b98
João Pedro G. M. Silva <joaopedrogmoraissilva@gmail.com>
2024-12-10 09:37:25 -0300

    added link and style in dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
 export default function Page() {
-    return <h1 className="text-3xl">Hello, Next.js!</h1>
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Welcome to ToDoList</h1>
+            <Link className="hover:underline hover:text-blue-500 text-2xl" href="/dashboard/todo">Dashboard</Link>
+        </div>
+    );
 }
